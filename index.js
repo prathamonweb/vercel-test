440a41b556fc48ed3dedd60cda4ad70ffd5a5a16
prathamaonweb <prathamonweb@gmail.com>
2022-12-28 02:11:51 +0530

    homepage

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require('express')
-
-const app = express()
+const bodyParser=require('body-parser')
 const PORT = 4000
+const app = express()
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+let homeContent= 'lorem';
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
+  res.render('home' ,{homeContent:homeContent})
 })
 
 app.get('/about', (req, res) => {
   res.send('This is my about route..... ')
 })
 
+app.listen(PORT, () => {
+    console.log(`API listening on PORT ${PORT} `)
+  })
+  
 // Export the Express API
 module.exports = app
